57453ec364a7ba48695e004ebf433cdafbf30809
Gopal Patel <nixme@stillhope.com>
2011-04-06 09:13:55 -0700

    Fix typo in IndexSong job

--- app/jobs/index_song.rb
+++ app/jobs/index_song.rb
@@ -3,6 +3,6 @@ class IndexSong
 
   def self.perform(song_id)
     song = Song[song_id]
-    Sunspot.index!(song) if Song   # index song into Solr via sunspot
+    Sunspot.index!(song) if song   # index song into Solr via sunspot
   end
 end
