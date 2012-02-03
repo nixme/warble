25e843356129177b2cb820fbb1c8a357d4c40936
Gopal Patel <nixme@stillhope.com>
2012-02-03 14:34:52 -0800

    Fix Pandora song downloading

--- app/jobs/archive_song.rb
+++ app/jobs/archive_song.rb
@@ -4,7 +4,7 @@ class ArchiveSong
   @queue = :download
 
   def self.perform(song_id)
-    song = Song[song_id]
+    song = Song.find(song_id)
     if song
       raise "No URL!" unless song.url    # check for an actual URL
       filename = Rails.root.join('public', 'songs', "#{song_id}.mp3").to_s
