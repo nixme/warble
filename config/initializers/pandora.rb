07fa08133fb9d7b7ed60979a90804e69d675f5e9
Gopal Patel <nixme@stillhope.com>
2012-09-25 15:05:38 -0700

    Only create Pandora client if keys available in environment

--- config/initializers/pandora.rb
+++ config/initializers/pandora.rb
@@ -1,7 +1,11 @@
-$pandora_partner = Pandora::Partner.new(
-  ENV['PANDORA_USERNAME'],
-  ENV['PANDORA_PASSWORD'],
-  ENV['PANDORA_DEVICE_ID'],
-  ENV['PANDORA_ENCRYPTION_KEY'],
-  ENV['PANDORA_DECRYPTION_KEY']
-)
+# Create a shared Pandora partner client if keys are available.
+
+if ENV['PANDORA_USERNAME'].present?
+  $pandora_partner = Pandora::Partner.new(
+    ENV['PANDORA_USERNAME'],
+    ENV['PANDORA_PASSWORD'],
+    ENV['PANDORA_DEVICE_ID'],
+    ENV['PANDORA_ENCRYPTION_KEY'],
+    ENV['PANDORA_DECRYPTION_KEY']
+  )
+end
