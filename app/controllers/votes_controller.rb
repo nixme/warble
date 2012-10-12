cb17d348047bc691440eef1b57d48cc07bf6e4e7
Gopal Patel <nixme@stillhope.com>
2012-10-12 16:39:19 -0700

    Roboto webfont

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -4,10 +4,10 @@ class VotesController < ApplicationController
   def create
     begin
       song = Song.find(params[:song_id])
-      vote = song.votes.build()
+      vote = song.votes.build
       vote.user = current_user
       if vote.save
-        render json:   song.as_json(),
+        render json:   song.as_json,
                status: :created
       else
         head :forbidden
